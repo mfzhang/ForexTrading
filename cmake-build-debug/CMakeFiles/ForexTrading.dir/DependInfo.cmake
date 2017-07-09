639196570216d2e141282c4a38bdbe5d33d54a3c
Emilio Singh <u14006512@tuks.co.za>
2017-07-10 00:37:52 +0200

    Working PSO
    PSO logic is done-Needs fitness module now

---
+++ cmake-build-debug/CMakeFiles/ForexTrading.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/DataStore.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/DataStore.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/IndicatorManager.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/IndicatorManager.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/OHLC.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/OHLC.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/ParameterParticle.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/ParameterParticle.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/ParameterSwarm.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/ParameterSwarm.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/Particle.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/Particle.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/StrategyParticle.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/StrategyParticle.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/StrategySwarm.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/StrategySwarm.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/Swarm.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/Swarm.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/Tick.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/Tick.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/WeightVectorParticle.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/WeightVectorParticle.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/WeightVectorSwarm.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/WeightVectorSwarm.cpp.o"
+  "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/main.cpp" "/cygdrive/c/Users/Warmaster/ClionProjects/ForexTrading/cmake-build-debug/CMakeFiles/ForexTrading.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ForexTrading.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ForexTrading.dir/DataStore.cpp.o"
+  "CMakeFiles/ForexTrading.dir/IndicatorManager.cpp.o"
+  "CMakeFiles/ForexTrading.dir/main.cpp.o"
+  "CMakeFiles/ForexTrading.dir/OHLC.cpp.o"
+  "CMakeFiles/ForexTrading.dir/ParameterParticle.cpp.o"
+  "CMakeFiles/ForexTrading.dir/ParameterSwarm.cpp.o"
+  "CMakeFiles/ForexTrading.dir/Particle.cpp.o"
+  "CMakeFiles/ForexTrading.dir/StrategyParticle.cpp.o"
+  "CMakeFiles/ForexTrading.dir/StrategySwarm.cpp.o"
+  "CMakeFiles/ForexTrading.dir/Swarm.cpp.o"
+  "CMakeFiles/ForexTrading.dir/Tick.cpp.o"
+  "CMakeFiles/ForexTrading.dir/WeightVectorParticle.cpp.o"
+  "CMakeFiles/ForexTrading.dir/WeightVectorSwarm.cpp.o"
+  "ForexTrading.pdb"
+  "ForexTrading.exe"
+  "libForexTrading.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ForexTrading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
